10aec0bceec805ac7a4f46fc2ce4371756ff6362
Pollutedlake <psh950827@naver.com>
2021-01-16 21:19:20 +0900

    detect InterativeObject

---
+++ portfolio1/Assets/Scripts/DetectInteractiveObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectInteractiveObject : MonoBehaviour
+{
+    public FireFlies fireFliesPrefab;
+    private Color originColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("InteractiveObject"))
+        {
+            Debug.Log(other.gameObject.name);
+            originColor = other.GetComponent<SpriteRenderer>().color;
+            FireFlies fireFly = Instantiate(fireFliesPrefab);
+            fireFly.transform.position = this.transform.position;
+            fireFly.target = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("InteractiveObject"))
+        {
+            other.GetComponent<SpriteRenderer>().color = originColor;
+        }
+    }
+}
--- portfolio1/Assets/Scripts/FireFlies.cs
+++ portfolio1/Assets/Scripts/FireFlies.cs
@@ -6,20 +6,73 @@ public class FireFlies : MonoBehaviour
 {
     public Character character;
     public Vector3 characterPosition;
+    public GameObject target;
+
+    public float firingAngle = 45.0f;
+    public float gravity = 9.8f;
+
     private void Start()
     {
         if (character == null)
         {
             character = FindObjectOfType<Character>();
         }
+        if (target.CompareTag("InteractiveObject"))
+        {
+            StartCoroutine(DetectInteractiveObject());
+        }
     }
 
     private void Update()
     {
-        characterPosition = new Vector3(character.transform.position.x, transform.position.y, character.transform.position.z);
-        if ((characterPosition - transform.position).sqrMagnitude < 0.1f || (characterPosition - transform.position).sqrMagnitude > 25.0f)
+        if (target.CompareTag("InteractiveObject"))
         {
-            this.gameObject.SetActive(false);
+            
+            Vector3 distance = target.transform.position - transform.position;
+            if (distance.sqrMagnitude < 0.1f)
+            {
+                target.GetComponent<SpriteRenderer>().color = Color.green;
+                Destroy(gameObject);
+            }
+            
+        }
+        else
+        {
+            characterPosition = new Vector3(character.transform.position.x, transform.position.y, character.transform.position.z);
+            if ((characterPosition - transform.position).sqrMagnitude < 0.1f || (characterPosition - transform.position).sqrMagnitude > 25.0f)
+            {
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    IEnumerator DetectInteractiveObject()
+    {
+        // Calculate distance to target
+        float target_Distance = Vector3.Distance(transform.position, target.transform.position);
+
+        // Calculate the velocity needed to throw the object to the target at specified angle.
+        float projectile_Velocity = target_Distance / (Mathf.Sin(2 * firingAngle * Mathf.Deg2Rad) / gravity);
+
+        // Extract the X  Y componenent of the velocity
+        float Vx = Mathf.Sqrt(projectile_Velocity) * Mathf.Cos(firingAngle * Mathf.Deg2Rad);
+        float Vy = Mathf.Sqrt(projectile_Velocity) * Mathf.Sin(firingAngle * Mathf.Deg2Rad);
+
+        // Calculate flight time.
+        float flightDuration = target_Distance / Vx;
+
+        // Rotate projectile to face the target.
+        transform.rotation = Quaternion.LookRotation(target.transform.position - transform.position);
+
+        float elapse_time = 0;
+
+        while (elapse_time < flightDuration)
+        {
+            transform.Translate(0, (Vy - (gravity * elapse_time)) * Time.deltaTime, Vx * Time.deltaTime);
+
+            elapse_time += Time.deltaTime;
+
+            yield return null;
         }
     }
 }
